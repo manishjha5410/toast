3533f7fc8b359cd6aad4596a75306f006e4c9be1
manishjha5410 <manishjha5410@gmail.com>
2021-04-03 14:10:54 +0530

    Working on some feature

--- input/input.js
+++ input/input.js
@@ -1,1 +1,18 @@
-document.get
+window.onload = () => {
+    document.querySelector(".btn-outline.primary").onclick = function() {
+        toast('top-right','This is a <a href="#" style="text-decoration: none;color: unset;">info</a> text','info',false,4000,50);
+    }
+
+    document.querySelector(".btn-outline.success").onclick = function() {
+        toast('top-right','This is a <a href="#" style="text-decoration: none;color: unset;">success</a> text','success',false,4000,50);
+    }
+
+    document.querySelector(".btn-outline.warning").onclick = function() {
+        toast('top-right','This is a <a href="#" style="text-decoration: none;color: unset;">warning</a> text','warning',false,4000,50);
+    }
+
+    document.querySelector(".btn-outline.danger").onclick = function() {
+        toast('top-right','This is a <a href="#" style="text-decoration: none;color: unset;">error</a> text','error',false,4000,50);
+    }
+}
+
